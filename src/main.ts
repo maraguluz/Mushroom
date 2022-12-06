6ee1a1a67a9f823e68f96922d0be13375dfba37a
Mara Guluzian <mara15.guluzian@gmail.com>
2022-12-05 22:59:58 -0500

    Made button clickable and display photo

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,20 @@
 import './style.css'
+
+let button = document.getElementById('button') as HTMLButtonElement
+let image = document.getElementById('image') as HTMLImageElement
+
+const apiUrl =
+  'https://api.nasa.gov/planetary/apod?api_key=ysKFxVTNsi8xY6enhZwXdyKCIiUvdpu2L3InXPry'
+
+button.addEventListener('click', () => {
+  fetch(apiUrl, {
+    method: 'GET',
+  })
+    .then((response) => response.json())
+    .then((result) => {
+      console.log(result)
+      image.src = result.url
+    })
+    .catch((err) => console.log(err))
+})
+//getting error image is null
